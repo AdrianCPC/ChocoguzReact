9f16282342e318bdfc9038a4be29434470453748
AdrianPC <adrianchooo18@gmail.com>
2021-09-07 21:00:56 -0500

    creacion navbar y logo menuhamgurgesa

--- src/components/layout/Header/Navbar.elements.jsx
+++ src/components/layout/Header/Navbar.elements.jsx
@@ -20,3 +20,55 @@ display: flex;
 flex-wrap: wrap;
 justify-content: space-between;
 `;
+
+//Making icono for navbar
+export const Iconlogo = styled.div`
+display: flex;
+align-items: center;
+flex-wrap: wrap;
+justify-content: center;
+font-size: 0.2em;
+`;
+
+//Making menu for navbar
+export const Menu = styled.ul`
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+export const MenuItem = styled.li`
+height: 100%;
+padding: 0.5rem 1.5rem;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+font-family: 'Montserrat', sans-serif;
+font-weight: normal;
+
+//Hover menu
+&:hover{
+    background-color: #eaeaea;
+    border-bottom: 2px solid #3d1708;
+    transition: 0.4s ease-in;
+}
+`;
+
+export const MenuItemLink = styled.a`
+text-decoration: none;
+color: #3d1708;
+`;
+
+//Making Mobile mode
+export const IconLogoMobile = styled.div`
+display: none;
+
+//Media query
+@media screen and (max-width: 768px) {
+    display: flex;
+    color: #3d1708;
+    font-size: 2rem;
+}
+`;
+
--- src/components/layout/Header/Navbar.jsx
+++ src/components/layout/Header/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Iconlogo, IconLogoMobile, Menu, MenuItem, MenuItemLink, NavbarContainer, NavbarWrapper } from './Navbar.elements'
+import ChocoGuz from '../../../assets/icons/ChocoGuz.svg'
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
         <>
-            <h1>Hola</h1>
+        <NavbarContainer>
+            <NavbarWrapper>
+                <Iconlogo>
+                    <img src={ChocoGuz} alt="logo" />
+                </Iconlogo>
+                <IconLogoMobile>
+                    <FaBars />
+                </IconLogoMobile>
+                <Menu>
+                    <MenuItem>
+                    <MenuItemLink>
+                    INICIO
+                    </MenuItemLink>
+                    </MenuItem>
+                    <MenuItem>
+                    <MenuItemLink>
+                    TIENDA
+                    </MenuItemLink>
+                    </MenuItem>
+                    <MenuItem>
+                    <MenuItemLink>
+                    CARRITO
+                    </MenuItemLink>
+                    </MenuItem>
+                </Menu>
+            </NavbarWrapper>
+        </NavbarContainer>
         </>
     )
 }
