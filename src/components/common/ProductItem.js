418128e16c784a42043b935890f8b10492a1e17c
AdrianPC <adrianchooo18@gmail.com>
2021-11-23 23:43:24 -0500

    second methos doesnt still work

--- src/components/common/ProductItem.js
+++ src/components/common/ProductItem.js
@@ -3,12 +3,14 @@ import React from 'react';
 function ProductItem({image, name, price}) {
     return (
         <div className="product-item">
-            <div>{image}</div>
+            <div className="img__container">
+                <img src={require('../../assets/images/Clavos.png')} alt='images'/>
+            </div> 
             {/* <div style={{backgroundImage: `url(${image})`}}></div> */}
             <h1>{name}</h1>
             <p>${price}</p>
         </div>
     )
-}
+};
 
-export default ProductItem
+export default ProductItem;
